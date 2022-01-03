e4b1f290eace912837e8ce957407deb54e8fe30a
vsm <vsm.github@gmail.com>
2022-01-03 14:54:09 +0200

    Added classes for command

---
+++ 06.1_Command/README.md
@@ -0,0 +1,5 @@
+# Command
+
+A design pattern that allows you to encapsulate a request as an object, allowing you to parameterize clients with different requests, queue or log requests, and support undoable operations
+
+![](command.png)
---
+++ 06.1_Command/src/com/codewithmosh/commandtest/Main.java
@@ -0,0 +1,16 @@
+package com.codewithmosh.commandtest;
+
+import com.codewithmosh.command.AddCustomerCommand;
+import com.codewithmosh.command.CustomerService;
+import com.codewithmosh.command.fx.Button;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var service = new CustomerService();
+		var command = new AddCustomerCommand(service);
+		var button = new Button(command);
+		button.click();
+	}
+
+}

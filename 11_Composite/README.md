7e89e1c940e05a729143e2cf373d68f3db690b2a
vsm <vsm.github@gmail.com>
2022-01-04 13:11:52 +0200

    Added classes for composite

---
+++ 11_Composite/README.md
@@ -0,0 +1,5 @@
+# Composite
+
+A design pattern that lets you compose objects into a tree structure & subesquently work with these structures as if they were individual objects.
+
+![](composite.png)
---
+++ 11_Composite/src/composite/Group.java
@@ -0,0 +1,26 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group implements Component {
+	private List<Component> components = new ArrayList<>();
+
+	public void add(Component shape) {
+		this.components.add(shape);
+	}
+	
+	@Override
+	public void render() {
+		for (var component : components) {
+			component.render();
+		}
+	}
+
+	@Override
+	public void move() {
+		for (var component : components) {
+			component.move();
+		}
+	}
+}
